4267edc50e399f90d142522bab61720a696a127c
Christian Glessner <cg@expertsinside.com>
2018-11-08 22:42:31 +0100

    Better tabs end project / more than covered in the lab

--- src/tabs/end/Program.cs
+++ src/tabs/end/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace content
+namespace myteamsapp
 {
     public class Program
     {
